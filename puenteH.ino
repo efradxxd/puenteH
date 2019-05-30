d1bd485a223cef30a68879a153636acc5df90b38
Jesus Lerma Carrera <jlerma@firstmajestic.com>
2019-05-30 09:43:15 -0500

    fix

--- puenteH.ino
+++ puenteH.ino
@@ -1,33 +1,41 @@
 const int controlPin1 =  2;
-const int controlPin2 = 2;
+const int controlPin2 = 3;
+const int controlPin4 = 4;
+const int controlPin5 = 5;
 const int enablePin = 9;
+const int enablePin2 = 10;
 const int directionSwitchPin = 4;
 const int onOffSwitchStateSwitchPin = 5;
 const int potPin = A0;
 
-int onOffSwitchState = 0;
+int onOffSwitchState = 1;
 int previousOnOffSwitchState = 0;
-int directionSwitchState = 0;
+int directionSwitchState = 1;
 int previousDirectionSwitchState = 0;
 
 int motorEnabled = 0;
 int motorSpeed = 0;
-int motorDirection = 1;
+int motorDirection = 0;
 
 
 void setup() {
-  pinMode(directionSwitchPin, INPUT);
-  pinMode(onOffSwitchStateSwitchPin,INPUT);
+  //pinMode(directionSwitchPin, INPUT);
+  //pinMode(onOffSwitchStateSwitchPin,INPUT);
   pinMode(controlPin1, OUTPUT);
   pinMode(controlPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
-  digitalWrite(enablePin, LOW);
+  pinMode(controlPin4, OUTPUT);
+  pinMode(controlPin5, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, HIGH);
+  digitalWrite(enablePin2, HIGH);
 }
 
 void loop() {
-  onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+  //onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
   delay(1);
-  directionSwitchState = digitalRead(directionSwitchPin);
+  //directionSwitchState = digitalRead(directionSwitchPin);
+  directionSwitchState = 1;
   //motorSpeed = analogRead(potPin)/4;
 
   if(onOffSwitchState != previousOnOffSwitchState){
@@ -45,10 +53,14 @@ void loop() {
   if (motorDirection == 1){
     digitalWrite(controlPin1,HIGH);
     digitalWrite(controlPin2,LOW);
+    digitalWrite(controlPin4,HIGH);
+    digitalWrite(controlPin5,LOW);
   }
   else{
     digitalWrite(controlPin1, LOW);
     digitalWrite(controlPin2, HIGH);
+    digitalWrite(controlPin4,LOW);
+    digitalWrite(controlPin5,HIGH);
   }
 
   /*if (motorEnabled ==1){
